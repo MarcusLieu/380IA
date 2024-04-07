54c7a24e8e345c45bcb075b444591e8d90e8700d
MarcusLieu <marcus.lieu4@gmail.com>
2024-04-06 22:05:07 -0600

    started building disaster victim interface

--- edu/ucalgary/oop/CLI.java
+++ edu/ucalgary/oop/CLI.java
@@ -1,7 +1,49 @@
 package edu.ucalgary.oop;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CLI {
+    public static void main(String[] args) {
+        Location siteA = new Location("siteA", "123 street NE");
+        Location siteB = new Location("siteB", "123 street NE");
+        Location siteC = new Location("siteC", "123 street NE");
+        Location siteD = new Location("siteD", "123 street NE");
+
+        ArrayList<Location> sites = new ArrayList<Location>();
+        sites.add(siteA);
+        sites.add(siteB);
+        sites.add(siteC);
+        sites.add(siteD);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("--------------------------------------------------");
+            System.out.println("Welcome to DisasterVictim CLI");
+            System.out.println("1.) view available locations");
+            System.out.println("2.) view occupants at location");
+            System.out.println("3.) add a disaster victim to system");
+            System.out.println("4.) exit");
+            System.out.print("Type number corresponding to action to select: ");
+            try {
+                int choice = scanner.nextInt();
+                if (choice == 1) {
+                    System.out.println("--------------------------------------------------");
+                    System.out.println("Available locations: ");
+                    for (int i = 0; i < sites.size(); i++) {
+                        System.out.println(sites.get(i).getName());
+                    }
+                }
+                if (choice == 4) {
+                    System.out.println("exiting program.");
+                    break;
+                }
+            }
+            catch(Exception e) {
+                System.out.println("Invalid choice");
+            }
+        }
+        scanner.close();
+
+    }
 }
